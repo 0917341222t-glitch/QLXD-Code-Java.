5b5b34e77f7eb10525add6d266e5acf62d3aa897
Thanh Tran <0917341222t@gmail.com>
2026-05-01 11:17:59 +0700

    Create KhachHang.java

---
+++ KhachHang.java
@@ -0,0 +1,64 @@
+package model;
+
+public class KhachHang {
+   private int maKH;
+   private String hoTen;
+   private String soDienThoai;
+   private String diaChi;
+   private String email;
+
+   public KhachHang() {
+   }
+
+   public KhachHang(int var1, String var2, String var3, String var4, String var5) {
+      this.maKH = var1;
+      this.hoTen = var2;
+      this.soDienThoai = var3;
+      this.diaChi = var4;
+      this.email = var5;
+   }
+
+   public int getMaKH() {
+      return this.maKH;
+   }
+
+   public void setMaKH(int var1) {
+      this.maKH = var1;
+   }
+
+   public String getHoTen() {
+      return this.hoTen;
+   }
+
+   public void setHoTen(String var1) {
+      this.hoTen = var1;
+   }
+
+   public String getSoDienThoai() {
+      return this.soDienThoai;
+   }
+
+   public void setSoDienThoai(String var1) {
+      this.soDienThoai = var1;
+   }
+
+   public String getDiaChi() {
+      return this.diaChi;
+   }
+
+   public void setDiaChi(String var1) {
+      this.diaChi = var1;
+   }
+
+   public String getEmail() {
+      return this.email;
+   }
+
+   public void setEmail(String var1) {
+      this.email = var1;
+   }
+
+   public String toString() {
+      return this.hoTen;
+   }
+}
